9f062260a45dd052acf7a4a3cd80d881346beff8
h586601 <586601@stud.hvl.no>
2021-03-12 21:32:03 +0100

    VoteDAO created with one method.

--- Scan Your Stand/src/vote/VoteDAO.java
+++ Scan Your Stand/src/vote/VoteDAO.java
@@ -1,5 +1,37 @@
 package vote;
 
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+/**
+ * Defines the Data Access Object for Vote
+ * 
+ * @author Ida
+ *
+ */
+@Stateless
 public class VoteDAO {
+	
+	@PersistenceContext(name = "scanyourstandUP")
+	private EntityManager em;
+
+	
+	/**
+	 * Returns all votes
+	 * 
+	 * @return List<Vote>
+	 */
+	public List<Vote> getAllVotes() {
+		
+		String jpql = "SELECT v from VOTE v";	
+		TypedQuery<Vote> query = em.createQuery(jpql, Vote.class);
+		
+	    return query.getResultList();
+		
+	}
 
 }
