176c2b7f8d2d446055bad25e067abd4cf8fce06f
olive <olive@DESKTOP-R044BGP>
2021-03-14 22:45:17 +0100

    Implemented a first look at Admin class

--- Scan Your Stand/src/admin/Admin.java
+++ Scan Your Stand/src/admin/Admin.java
@@ -1,7 +1,25 @@
 package admin;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import utils.BCrypt;
+
+@Entity
+@Table(schema = "scanyourstand")
 public class Admin {
 
-	public String getHashedPassword;
+	@Id
+	private String username;
+	private String hashedPassword;
+	
+	public Admin(String username, String password) {
+		this.username = username;
+		this.hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt(12));
+	}
+	
+	public String getHashedPassword() {
+		return hashedPassword;
+	}
 
 }
