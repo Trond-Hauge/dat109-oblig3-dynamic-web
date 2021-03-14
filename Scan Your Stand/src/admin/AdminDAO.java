ff1aa8ad9b601d87d14f9c91852abcbecf2ff45f
h586601 <586601@stud.hvl.no>
2021-03-14 22:39:55 +0100

    AdminDAO with findAdmin(String username)

--- Scan Your Stand/src/admin/AdminDAO.java
+++ Scan Your Stand/src/admin/AdminDAO.java
@@ -1,10 +1,23 @@
 package admin;
 
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless
 public class AdminDAO {
 
+	@PersistenceContext(name = "scanyourstand")
+	EntityManager em;
+	
+	/**
+	 * Find the admin by username
+	 * 
+	 * @param username
+	 * @return
+	 */
 	public Admin findAdmin(String username) {
-		// TODO Auto-generated method stub
-		return null;
+		return em.find(Admin.class, username);
 	}
 
 }
