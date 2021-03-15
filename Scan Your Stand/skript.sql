bd2f5b38295b3978a49c679d4df71f636274ca22
h586601 <586601@stud.hvl.no>
2021-03-15 01:05:04 +0100

    Updated SQL document, includes Stand, Vote and Admin.

--- Scan Your Stand/skript.sql
+++ Scan Your Stand/skript.sql
@@ -4,10 +4,34 @@ SET search_path = ScanYourStand;
 
 CREATE TABLE Stand
 (
-standID		VARCHAR(10) UNIQUE,
-standName	VARCHAR(20)	
+-- Assuming length of standID is always 4
+standID			CHARACTER (4) CHECK (char_length(standID) = 4),
+standName		CHARACTER VARYING (20),
+CONSTRAINT StandPK PRIMARY KEY (standID)
 );
 
-INSERT INTO Stand (standName) VALUES
-('Stand1'),
-('Stand2');
+INSERT INTO Stand (standID, standName) VALUES
+('M123', 'Stand1'),
+('B123', 'Stand2');
+
+CREATE TABLE Vote
+(
+--Assuming length of phone is always 8
+phone		CHARACTER (8) CHECK (char_length(phone) = 8),
+standID		CHARACTER VARYING (10),
+points		INTEGER CHECK (0 <= points AND points <= 5),
+CONSTRAINT VotePK PRIMARY KEY (phone, standID),
+CONSTRAINT StandFK FOREIGN KEY (standID) REFERENCES stand (standID)
+);
+
+INSERT INTO Vote (phone, standID, points) VALUES
+('97084676', 'M123', 5),
+('95991519', 'M123', 2),
+('97084676', 'B123', 4);
+
+CREATE TABLE Admin
+(
+username		CHARACTER VARYING (20),
+hashedPassword	CHARACTER VARYING (50), --Assuming no set length
+CONSTRAINT AdminPK PRIMARY KEY (username)
+);
