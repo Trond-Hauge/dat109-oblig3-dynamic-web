7853998e0754022eeefa5017d78e0ba1da2d0083
h586601 <586601@stud.hvl.no>
2021-03-24 17:43:50 +0100

    ...

--- Scan Your Stand/src/project/ProjectDAO.java
+++ Scan Your Stand/src/project/ProjectDAO.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
-import javax.persistence.Persistence;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
