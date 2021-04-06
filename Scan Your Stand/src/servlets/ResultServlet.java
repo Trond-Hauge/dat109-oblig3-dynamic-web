252c7a3ac3958a6b1831bf9ce2dafdcfa4bf730e
olive <olive@LAPTOP-MAKEJ906>
2021-04-06 11:05:15 +0200

    Result page is now expo-specific

--- Scan Your Stand/src/servlets/ResultServlet.java
+++ Scan Your Stand/src/servlets/ResultServlet.java
@@ -9,6 +9,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import exhibition.Exhibition;
+import exhibition.ExhibitionDAO;
 import project.Project;
 import project.ProjectDAO;
 import utils.VoteUtils;
@@ -26,19 +29,48 @@ public class ResultServlet extends HttpServlet {
 	@EJB
 	private ProjectDAO projectDAO;
 	
+	@EJB
+	private ExhibitionDAO exhibitionDAO;
+	
 	// Readies data before forwarding to result.jsp
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		List<Vote> votes = voteDAO.getAllVotes();
-		List<Project> projects = projectDAO.getAllProjects();
+		Exhibition exhibition = null;
 		
-		// Gets the sorted project score map
-		Map<String,Integer> resultMap = VoteUtils.getSortedProjectScoreMap(projects, votes);
+		try {
+			int id = Integer.parseInt(request.getParameter("id"));
+			exhibition = exhibitionDAO.findExhibitionById(id);
+		}
+		catch(Exception e) {}
 		
-		// Sets the sorted map as a request attribute
-		request.setAttribute("resultMap", resultMap);
+		if(exhibition == null) {
+			List<Exhibition> exhibitions = exhibitionDAO.getAllExhibitions();
+			request.setAttribute("exhibitions", exhibitions);
+			request.getRequestDispatcher("WEB-INF/view-results.jsp").forward(request, response);
+		}
+		else {
+			List<Vote> votes = voteDAO.getAllVotes();
+			List<Project> projects = exhibition.getProjects();
+			
+			// Gets the sorted project score map
+			Map<String,Integer> resultMap = VoteUtils.getSortedProjectScoreMap(projects, votes);
+			
+			// Sets the sorted map as a request attribute
+			request.setAttribute("resultMap", resultMap);
+			
+			// Forwards to result.jsp
+			request.getRequestDispatcher("WEB-INF/view-results.jsp").forward(request, response);	
+		}
 		
-		// Forwards to result.jsp
-		request.getRequestDispatcher("WEB-INF/view-results.jsp").forward(request, response);	
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		String id = request.getParameter("id");
+		
+		response.sendRedirect("result?id=" + id);
+		
 	}
 }
